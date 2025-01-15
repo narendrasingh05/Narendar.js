7c3cf956a25fd3b8b9b60dac0b4791620066f9cc
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-15 17:50:53 +0000

    container config done

---
+++ javascript/variable.js
@@ -0,0 +1,12 @@
+const accountId = "1445566"
+let accountName = "narendar"
+var accountEmail = "nk@gamail.com"
+let accountPassword = "123456";
+
+accountCity = "bahraich";
+
+//accountId = 2345;
+
+//console.log(accountId);
+
+console.table([accountId, accountName, accountEmail, accountPassword, accountCity]);
