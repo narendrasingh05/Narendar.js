f4955fbc0dceb54ec9ee09ddb8acbbca2e293d8a
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-16 08:06:54 +0000

    container config done

---
+++ javascript/datatype.js
@@ -0,0 +1,38 @@
+"use srict" //treat all js code as newer version.
+
+//alert(3 + 3); we are using nodejs not browser.
+
+/*
+console.log(3 + 
+    3)
+    code readability should be high, this methode is not good */
+
+     console.log(3 + 3)
+
+    console.log("narendar");
+
+let name = "narendar singh"
+let age = 18
+let islogin = true/false;
+let state;
+
+//number => 2 to power 53
+
+//BigInt => 
+
+//String => ""
+
+//Boolean => true/false
+
+//null => standalone value
+
+//undefined => 
+
+//symble => unique
+
+//object =>
+    console.log(typeof "narendar singh")
+console.log(typeof 18)
+console.log(typeof true)
+console.log(typeof state)
+
