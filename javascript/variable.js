70873b985a1b26c9f26d7c6fd6c1db3000d32f0a
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-16 07:27:23 +0000

    container config done

--- javascript/variable.js
+++ javascript/variable.js
@@ -3,10 +3,22 @@ let accountName = "narendar"
 var accountEmail = "nk@gamail.com"
 let accountPassword = "123456";
 
-accountCity = "bahraich";
+accountCity = "bahraich"; //this method is not godd
 
-//accountId = 2345;
+//accountId = 2345; //not allowed
 
 //console.log(accountId);
 
-console.table([accountId, accountName, accountEmail, accountPassword, accountCity]);
+//console.table([accountId, accountName, accountEmail, accountPassword, accountCity]);
+
+accountEmail = "ak@gmail.com";
+accountName ="abhay";
+accountPassword = "654321";
+accountCity = "unnao";
+let accountState;
+
+
+console.table([accountId, accountEmail, accountName, accountPassword,accountCity, accountState]);
+
+/*prefer nat to use var because of issue in bloke scope and functional scope */
+
