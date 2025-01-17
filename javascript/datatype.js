fdc4626f57bc55d9f5b87d1f62cda4579f4a8964
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-17 13:08:34 +0000

    container config done

--- javascript/datatype.js
+++ javascript/datatype.js
@@ -36,5 +36,11 @@ console.log(typeof 18)
 console.log(typeof true)
 console.log(typeof state)
 console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof Symbol())
+console.log(typeof {})
+console.log(typeof [])
+console.log(typeof function(){})
+
 
 
