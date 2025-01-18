a5221a358d206b7fdc48bc3520be34590e7d32b7
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-18 17:47:02 +0530

    Add files via upload

--- javascript/datatype.js
+++ javascript/datatype.js
@@ -37,9 +37,8 @@ console.log(typeof true)
 console.log(typeof state)
 console.log(typeof null)
 console.log(typeof undefined)
-console.log(typeof Symbol())
-console.log(typeof {})
-console.log(typeof [])
-console.log(typeof function(){})
+console.log(typeof Symbol("narendar"))
+console.log(typeof {name: "narendar", age: 18})
+console.log(typeof [1,2,3,4,5])
 
 
--- javascript/variable.js
+++ javascript/variable.js
@@ -3,23 +3,10 @@ let accountName = "narendar"
 var accountEmail = "nk@gamail.com"
 let accountPassword = "123456";
 
-accountCity = "bahraich"; //this method is not godd
+accountCity = "bahraich";
 
-//accountId = 2345; //not allowed
+//accountId = 2345;
 
 //console.log(accountId);
 
-//console.table([accountId, accountName, accountEmail, accountPassword, accountCity]);
-
-accountEmail = "ak@gmail.com";
-accountName ="abhay";
-accountPassword = "654321";
-accountCity = "unnao";
-let accountState;
-
-
-console.table([accountId, accountEmail, accountName, accountPassword,accountCity, accountState]);
-
-/*prefer nat to use var because of issue in bloke scope and functional scope */
-
-
+console.table([accountId, accountName, accountEmail, accountPassword, accountCity]);
