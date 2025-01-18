77b65f0c32876396b652aacf2ce09ca38dace4de
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-18 17:24:33 +0000

    conversion.js

--- javascript/conversion.js
+++ javascript/conversion.js
@@ -1,12 +1,11 @@
  let score = "33abc"
 
 
-// console.log(typeof score);
-// console.log(typeof (score));
+console.log(typeof score);
 
-// let valueInNumber = Number(score);
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 //"33" => 33
 // "33abc" => nan
@@ -16,4 +15,4 @@ let isLoggedin = 1;
 let booleanIsLoggedin = boolean(isLoggedin);
 console.log(booleanIsLoggedin);
 console.log(valueInNumber);
-
+console.log()
