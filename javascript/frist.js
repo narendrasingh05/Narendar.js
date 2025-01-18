cb5db5f2f9fd72315958122d15b98fe2f6098ddd
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-18 14:12:22 +0000

    frist.js

--- javascript/frist.js
+++ javascript/frist.js
@@ -6,6 +6,7 @@ x = null;
 y = undefined;
 isFollow = true;
 console.table([fullName, age, price, x, y, isFollow]);
+console.table([typeof fullName, typeof age, typeof price, typeof x, typeof y, typeof isFollow]);
 
 
 
