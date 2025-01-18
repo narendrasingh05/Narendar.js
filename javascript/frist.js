44e8d14d22b897623395c7b619076f55ea174238
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-18 14:16:10 +0000

    frist.js

--- javascript/frist.js
+++ javascript/frist.js
@@ -1,10 +1,10 @@
 //console.log("narendar singh");
-fullName = "narendar singh";
-age = 25;
-price =99.9;
-x = null;
-y = undefined;
-isFollow = true;
+fullName = "narendar singh"; //string
+age = 25; // number
+price =99.9; //number
+x = null; // number
+y = undefined; //undifined
+isFollow = true; //boolean
 console.table([fullName, age, price, x, y, isFollow]);
 console.table([typeof fullName, typeof age, typeof price, typeof x, typeof y, typeof isFollow]);
 
