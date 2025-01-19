23854bf692baaf9ef05232e2f067405001090953
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-19 08:34:55 +0000

    operation.js

---
+++ javascript/operation.js
@@ -0,0 +1,18 @@
+let value = 3;
+// let negativeValue = -value;
+// console.log(negativeValue); // -3
+
+// let str1 = "Hello";
+// let str2 = "World";
+// let str3 = str1 + " " + str2;
+// console.log(str3); // Hello World
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log(1 + 2 + 2); // 3
+// console.log(1 + 2 + "2"); // 32
+// console.log("1" + 2 + 2); // 122
+// console.log("1" + (2 + 2)); // 14
+// console.log(1 + 2 + "2" + 3 + 4); // 3234
+
+let x = 10;
