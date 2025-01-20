5012c5c76b54f144694716638a106f3ab698ffdf
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-01-20 01:06:49 +0000

    practice.js

---
+++ javascript/practice.js
@@ -0,0 +1,12 @@
+let identityCard = 123456789;{
+    fullName = "Narendar Singh";
+    fatherName = "Gokul Singh";
+    age = 19;
+    villege = "Lakhahi Putali Tara";
+    district = "Bahraich";
+    state = "Uttar Pradesh";
+    pincode = 271871;
+    console.table([identityCard, fullName, fatherName, age, villege, district, state, pincode]);
+}
+//you can print this code like this:
+//console.table("My name is " + fullName + ". My father's name is " + fatherName + ". I am " + age + " years old. I live in " + villege + ", " + district + ", " + state + ". My pincode is " + pincode + ".");
