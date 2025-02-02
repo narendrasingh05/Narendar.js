af41184586e90fbd0af6fb3af4f977684485d2ba
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-02-02 16:30:47 +0000

    variable.js config done

--- javascript/variable.js
+++ javascript/variable.js
@@ -1,2 +1,18 @@
-let fullName = "Abhay Singh";
+const accountId = 123456;
+let accountemail = "nk@gmail.com";
+var accountPassword = "654321";
+accountState; // Error: Uncaught ReferenceError: accountState is not defined
 
+
+//accountId = 234567; // Error: Assignment to constant variable.
+
+accountemail = "ak@gmail.com"; // No Error
+accountPassword = "123456"; // No Error
+accountCity = "Delhi"; // No Error
+
+console.table([accountId, accountemail, accountPassword, accountCity]);
+
+
+/*
+prefer not to use var because of issuse of functionl scope
+*/
