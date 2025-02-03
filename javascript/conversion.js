8fdd4deb233c133bf6b6657f2f3c806ec623802a
narendrasingh05 <narendarsingh.onlineindia@gmail.com>
2025-02-03 02:21:09 +0000

    conversion.js

--- javascript/conversion.js
+++ javascript/conversion.js
@@ -34,3 +34,43 @@ console.log(num3); // 1
 //NaN
 let num4 = Number('Hello');
 console.log(num4); // NaN
+
+//operations
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c); // 30
+
+let d = a - b;
+console.log(d); // -10
+
+let e = a * b;
+console.log(e); // 200
+
+let f = a / b;
+console.log(f); // 0.5
+
+let g = a % b;
+console.log(g); // 10
+
+let h = a ** b;
+console.log(h); // 100000000000000000000
+
+let i = a++;
+console.log(i); // 10
+
+let j = b--;
+console.log(j); // 20
+
+let k = a += b;
+console.log(k); // 30
+
+let l = a -= b;
+console.log(l); // 10
+
+let m = a *= b;
+console.log(m); // 200
+
+let str4 = str1 + str2;
+console.log(str4); // 100true
+
